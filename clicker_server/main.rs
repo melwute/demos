49a22da4ef4f8ae5420e53c47208d3d4504cbc2b
Sissy Yaques <sissyyaques@Nicoles-MacBook-Pro.local>
2022-05-22 11:29:58 -0500

    Moved clicker code to its own folder

--- clicker_server/main.rs
+++ clicker_server/main.rs
@@ -1,53 +1,44 @@
-
 use futures::TryStreamExt as _;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
 use hyper::{Method, StatusCode};
 use std::convert::Infallible;
+use std::error;
 use std::net::SocketAddr;
 #[macro_use]
 extern crate lazy_static;
 
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new("Hello, World".into()))
-}
-
 async fn hello_basic(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    let static_file = hyper_staticfile::Static::new("");
+
     let mut response = Response::new(Body::empty());
 
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/click") => {
             let clicks = get_clicks();
-            *response.body_mut() = Body::from(format!("clicks {}", clicks));
+
+            *response.body_mut() = Body::from(format!("{}\"clicks\":{}{}", '{', clicks, '}'));
         }
         (&Method::POST, "/click") => {
             inc_clicks(1);
             let clicks = get_clicks();
-            *response.body_mut() = Body::from(format!("clicks {}", clicks));
-        }
-        (&Method::POST, "/echo/reverse") => {
-            //lets just consume the entire thing before hand.
-            let full_body = hyper::body::to_bytes(req.into_body()).await?;
-            let reversed = full_body.iter().rev().cloned().collect::<Vec<u8>>();
 
-            *response.body_mut() = reversed.into();
-        }
-        (&Method::POST, "/echo/uppercase") => {
-            //efficent streaming
-            let mapping = req.into_body().map_ok(|chunk| {
-                chunk
-                    .iter()
-                    .map(|byte| byte.to_ascii_uppercase())
-                    .collect::<Vec<u8>>()
-            });
-
-            *response.body_mut() = Body::wrap_stream(mapping);
-        }
-        (&Method::POST, "/echo") => {
-            *response.body_mut() = req.into_body();
+            *response.body_mut() = Body::from(format!("{}\"clicks\":{}{}", '{', clicks, '}'));
         }
         _ => {
-            *response.status_mut() = StatusCode::NOT_FOUND;
+            let file_response = static_file.serve(req).await;
+
+            //I for the life of me cannot map this error correctly
+            match file_response {
+                Ok(response) => {
+                    return Ok(response);
+                }
+                Err(err) => {
+                    println!("Error serving file {:?}", err);
+                    *response.status_mut() = StatusCode::NOT_FOUND;
+                }
+            }
+            //*response.status_mut() = StatusCode::NOT_FOUND;
         }
     };
 
@@ -93,13 +84,3 @@ async fn main() {
         eprintln!("server error: {}", e);
     }
 }
-
-/*
-cargo.toml
-
-[dependencies]
-hyper = { version = "0.14", features = ["full"] }
-tokio = { version = "1", features = ["full"] }
-futures = "0.3"
-lazy_static = "1.4.0"
-*/
--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,3 @@
-use futures::TryStreamExt as _;
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
-use hyper::{Method, StatusCode};
-use std::convert::Infallible;
-use std::error;
-use std::net::SocketAddr;
-#[macro_use]
-extern crate lazy_static;
-
-async fn hello_basic(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    let static_file = hyper_staticfile::Static::new("");
-
-    let mut response = Response::new(Body::empty());
-
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/click") => {
-            let clicks = get_clicks();
-
-            *response.body_mut() = Body::from(format!("{}\"clicks\":{}{}", '{', clicks, '}'));
-        }
-        (&Method::POST, "/click") => {
-            inc_clicks(1);
-            let clicks = get_clicks();
-
-            *response.body_mut() = Body::from(format!("{}\"clicks\":{}{}", '{', clicks, '}'));
-        }
-        (&Method::POST, "/echo") => {
-            *response.body_mut() = req.into_body();
-        }
-        _ => {
-            let file_response = static_file.serve(req).await;
-
-            //I for the life of me cannot map this error correctly
-            match file_response {
-                Ok(response) => {
-                    return Ok(response);
-                }
-                Err(err) => {
-                    println!("Error serving file {:?}", err);
-                    *response.status_mut() = StatusCode::NOT_FOUND;
-                }
-            }
-            //*response.status_mut() = StatusCode::NOT_FOUND;
-        }
-    };
-
-    Ok(response)
-}
-
-use std::sync::Mutex;
-
-lazy_static! {
-    static ref global_clicks: Mutex<u128> = Mutex::new(0 as u128);
-}
-
-fn get_clicks() -> u128 {
-    let clicks = global_clicks.lock().unwrap();
-    return *clicks;
-}
-
-fn inc_clicks(amount: i32) {
-    let mut clicks = global_clicks.lock().unwrap();
-    *clicks += amount as u128;
-}
-
-#[tokio::main]
-async fn main() {
-    println!("Number of clicks {}", get_clicks());
-
-    // We'll bind to 127.0.0.1:3000
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-
-    // A `Service` is needed for every connection, so this
-    // creates one from our `hello_world` function.
-    let make_svc = make_service_fn(|_conn| async {
-        // service_fn converts our function into a `Service`
-        Ok::<_, Infallible>(service_fn(hello_basic))
-    });
-
-    let server = Server::bind(&addr).serve(make_svc);
-
-    println!("Listening on http://{}", addr);
-
-    // Run this server for... forever!
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
-    }
-}
 
 /*
 cargo.toml
@@ -98,7 +9,6 @@ futures = "0.3"
 lazy_static = "1.4.0"
 */
 
-/*
 use crate::demo_brickbreak::DemoBrickBreakState;
 use crate::demo_foragerclone::DemoForagerClone;
 use crate::demo_roguelike::DemoRoguelike;
@@ -202,5 +112,3 @@ impl ApplicationState {
         }
     }
 }
-
-*/
